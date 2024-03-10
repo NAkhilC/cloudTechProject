934383fd5eda5a0a395ceab08075a9595d211a49
akhilcn9 <akhilcn9@gmail.com>
2024-03-10 12:33:24 -0400

    jenkins

--- Dockerfile.prod
+++ Dockerfile.prod
@@ -5,5 +5,5 @@ ENV NODE_ENV=production
 COPY package*.json ./
 RUN npm install --only=production
 COPY . .
-EXPOSE 4000
+EXPOSE 3000
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // server.js
 const express = require("express");
 require("dotenv").config();
-const config = require("./config");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const apiUrl = process.env.API_URL || "https://api.example.com";
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`Hello World! ${process.env.API_KEY}`);
 });
 
 app.get("/data", async (req, res) => {
